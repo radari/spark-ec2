2d97c34c1368cdbf63a9c6264a3ed8513315ec3e
Reynold Xin <rxin@apache.org>
2014-09-17 19:11:51 -0700

    ganglia setup.

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -6,21 +6,14 @@ rm -rf /mnt/ganglia/rrds/*
 
 # Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
 rmdir /var/lib/ganglia/rrds
-ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
 
 # Make sure rrd storage directory has right permissions
 mkdir -p /mnt/ganglia/rrds
-chown -R ganglia:ganglia /mnt/ganglia/rrds
+chown -R nobody:nobody /mnt/ganglia/rrds
 
-# Install ganglia
-# TODO: Remove this once the AMI has ganglia by default
+ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
 
-GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
-
-if ! rpm --quiet -q $GANGLIA_PACKAGES; then
-  yum install -q -y $GANGLIA_PACKAGES;
-fi
-for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
-done
-wait
+/etc/init.d/gmetad restart
+/etc/init.d/gmond restart
+/root/spark-ec2/pssh.sh "/etc/init.d/gmond restart"
+/etc/init.d/httpd restart
